6875e71b92f63fc9c3f1d052e50f1837dede3a67
Szőke Tamás <tamas.szoke@trendency.hu>
2022-10-24 17:04:10 +0200

    Add lessons

--- script.js
+++ script.js
@@ -1,30 +1,109 @@
 let lessonWrapper = document.querySelector('.lesson-wrapper');
+let activeWord;
 
 function loadLesson()
 {
+    let lessonButtons = document.querySelector(".lesson-buttons");
+
     for (var key in words) {
         let word = words[key];
-        console.log(word);
-        console.log(word.title);
     
         let button = document.createElement("button");
     
-        button.setAttribute("data-audio", word.audio);
         button.addEventListener('click', function() {
-            playAudio(word.audio);
+            showLessonCard(word);
         });
         button.setAttribute("class", "btn");
         button.textContent = word.title;
     
-        lessonWrapper.append(button);
+        lessonButtons.append(button);
     }
 }
 
 loadLesson();
 
-function playAudio(url)
+function showLessonCard(word)
 {
-	let sound = new Audio(ABAI_BASE_URL + url);
+    activeWord = word;
+    
+    emptyLessonCard(word);
+
+    console.log(word.title);
+    console.log(word);
+
+    let lessonCard = document.querySelector(".lesson-card");
+    lessonCard.classList.remove("hidden");
+
+    playAudio();
+    populateLessonCard(word);
+}
+
+function emptyLessonCard(word)
+{
+    let lessonCard = document.querySelector(".lesson-card");
+    lessonCard.classList.add("hidden");
+    
+    lessonCard.querySelector(".title .play-again").removeEventListener("click", playAudio);
+    lessonCard.querySelector(".title .word").innerHTML = "";
+    lessonCard.querySelector(".title .transcription").innerHTML = "";
+    lessonCard.querySelector(".translation").innerHTML = "";
+    lessonCard.querySelector(".examples").innerHTML = "";
+    lessonCard.querySelector(".variants").innerHTML = "";
+}
+
+function populateLessonCard(word)
+{
+    let lessonCard = document.querySelector(".lesson-card");
+
+     lessonCard.querySelector(".title .play-again").addEventListener("click", playAudio);
+    lessonCard.querySelector(".title .word").innerHTML = word.title;
+    lessonCard.querySelector(".title .transcription").innerHTML = word.transcription;
+    lessonCard.querySelector(".translation").innerHTML = word.translations.eng;
+    
+    let examplesHolder = lessonCard.querySelector(".examples");
+    examplesHolder.classList.remove("hidden");
+
+    let examplesTitle = document.createElement("span");
+    examplesTitle.innerHTML = "Examples:";
+
+    let examplesContent = document.createElement("div");
+    examplesContent.classList.add("examples-content");
+    
+    let kazExample = document.createElement("div");
+    kazExample.innerHTML = word.examples.kaz;
+    
+    let engExample = document.createElement("div");
+    engExample.innerHTML = word.examples.eng;
+
+    examplesHolder.append(examplesTitle);
+    examplesHolder.append(examplesContent);
+
+    examplesContent.append(kazExample);
+    examplesContent.append(engExample);
+
+    let variantsHolder = lessonCard.querySelector(".variants");
+    variantsHolder.classList.remove("hidden");
+
+    let variantsTitle = document.createElement("span");
+    variantsTitle.innerHTML = "Variants:";
+
+    let variantsContent = document.createElement("div");
+    variantsContent.classList.add("variants-content");
+
+    for (let i = 0; i < 2; i++) {
+        let variantElement = document.createElement("div");
+        variantElement.innerHTML = word.variants.kaz[i] + " (" + word.variants.eng[i] + ")";
+
+        variantsContent.append(variantElement);
+    }
+
+    variantsHolder.append(variantsTitle);
+    variantsHolder.append(variantsContent);
+}
+
+function playAudio()
+{
+	let sound = new Audio(ABAI_BASE_URL + activeWord.audio);
 	sound.play();
 }
 
